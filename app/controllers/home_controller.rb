e8d6c5ae4db4c5ddfe15a2c2ba8b2a59d1caf766
= <zrsmead@gmail.com>
2017-08-04 16:26:37 -0700

    got jwt auth working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
-	def index
+	before_filter :authenticate_request!
 
+	def index
+		render json: {'logged_in' => true}
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'auth_user' => 'authentication#authenticate_user'
   get 'home' => 'home#index'
 end
